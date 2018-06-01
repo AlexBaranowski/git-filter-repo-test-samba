AlexBaranowski/git-filter-repo-test-samba

commit 383e1295dc7cfa8a3713a6769a3ef0f90671fca8

ldb_tdb: modify ltdb_delete_noindex() to take a struct ldb_message
This will make it easier to delete records with the GUID TDB key

Signed-off-by: Andrew Bartlett <abartlet@samba.org>
Reviewed-by: Garming Sam <garming@catalyst.net.nz>

ldb_tdb/ldb_index.c
@@ -263,17 +263,20 @@ static int ltdb_dn_list_store_full(struct ldb_module *module,
 	struct ldb_message *msg;
 	int ret;
 
-	if (list->count == 0) {
-		ret = ltdb_delete_noindex(module, dn);
-		if (ret == LDB_ERR_NO_SUCH_OBJECT) {
-			return LDB_SUCCESS;
-		}
-		return ret;
-	}
-
 	msg = ldb_msg_new(module);
 	if (!msg) {
 		return ldb_module_oom(module);
+	}
+
+	msg->dn = dn;
+
+	if (list->count == 0) {
+		ret = ltdb_delete_noindex(module, msg);
+		if (ret == LDB_ERR_NO_SUCH_OBJECT) {
+			talloc_free(msg);
+			return LDB_SUCCESS;
+		}
+		return ret;
 	}
 
 	if (ltdb->cache->GUID_index_attribute == NULL) {
@@ -292,7 +295,6 @@ static int ltdb_dn_list_store_full(struct ldb_module *module,
 		}
 	}
 
-	msg->dn = dn;
 	if (list->count > 0) {
 		struct ldb_message_element *el;
 

ldb_tdb/ldb_tdb.c
@@ -491,7 +491,8 @@ static int ltdb_add(struct ltdb_context *ctx)
   delete a record from the database, not updating indexes (used for deleting
   index records)
 */
-int ltdb_delete_noindex(struct ldb_module *module, struct ldb_dn *dn)
+int ltdb_delete_noindex(struct ldb_module *module,
+			const struct ldb_message *msg)
 {
 	void *data = ldb_module_get_private(module);
 	struct ltdb_private *ltdb = talloc_get_type(data, struct ltdb_private);
@@ -502,7 +503,7 @@ int ltdb_delete_noindex(struct ldb_module *module, struct ldb_dn *dn)
 		return LDB_ERR_UNWILLING_TO_PERFORM;
 	}
 
-	tdb_key = ltdb_key_dn(module, dn);
+	tdb_key = ltdb_key_dn(module, msg->dn);
 	if (!tdb_key.dptr) {
 		return LDB_ERR_OTHER;
 	}
@@ -535,7 +536,7 @@ static int ltdb_delete_internal(struct ldb_module *module, struct ldb_dn *dn)
 		goto done;
 	}
 
-	ret = ltdb_delete_noindex(module, dn);
+	ret = ltdb_delete_noindex(module, msg);
 	if (ret != LDB_SUCCESS) {
 		goto done;
 	}

ldb_tdb/ldb_tdb.h
@@ -138,7 +138,8 @@ TDB_DATA ltdb_key_dn(struct ldb_module *module, struct ldb_dn *dn);
 TDB_DATA ltdb_key_msg(struct ldb_module *module, const struct ldb_message *msg);
 int ltdb_store(struct ldb_module *module, const struct ldb_message *msg, int flgs);
 int ltdb_modify_internal(struct ldb_module *module, const struct ldb_message *msg, struct ldb_request *req);
-int ltdb_delete_noindex(struct ldb_module *module, struct ldb_dn *dn);
+int ltdb_delete_noindex(struct ldb_module *module,
+			const struct ldb_message *msg);
 int ltdb_err_map(enum TDB_ERROR tdb_code);
 
 struct tdb_context *ltdb_wrap_open(TALLOC_CTX *mem_ctx,

commit 4ac30c810bfe94f2e51ae477ac15ece06c1864e4

ldb: Fix memory leak on module context
Introduced in 383e1295dc7cfa8a3713a6769a3ef0f90671fca8

BUG: https://bugzilla.samba.org/show_bug.cgi?id=13459

Signed-off-by: Lukas Slebodnik <lslebodn@fedoraproject.org>
Reviewed-by: Andrew Bartlett <abartlet@samba.org>
Reviewed-by: Garming Sam <garming@catalyst.net.nz>

Autobuild-User(master): Andrew Bartlett <abartlet@samba.org>
Autobuild-Date(master): Fri Jun  1 11:10:24 CEST 2018 on sn-devel-144

ldb_tdb/ldb_index.c
@@ -612,9 +612,9 @@ static int ltdb_dn_list_store_full(struct ldb_module *module,
 	if (list->count == 0) {
 		ret = ltdb_delete_noindex(module, msg);
 		if (ret == LDB_ERR_NO_SUCH_OBJECT) {
-			talloc_free(msg);
-			return LDB_SUCCESS;
+			ret = LDB_SUCCESS;
 		}
+		talloc_free(msg);
 		return ret;
 	}
 