AlexBaranowski/git-filter-repo-test-samba

commit c6763e26cc2c741351a459bf4a83935628c0ff82

ldb: Remove "private_library" variable with just one user
Signed-off-by: Andrew Bartlett <abartlet@samba.org>
Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

wscript
@@ -88,8 +88,6 @@ def build(bld):
     if not 'LDB_PACKAGE_VERSION' in bld.env:
         bld.env.LDB_PACKAGE_VERSION = LDB_VERSION
 
-    private_library = not bld.env.ldb_is_public_library
-
     # we're not currently linking against the ldap libs, but ldb.pc.in
     # has @LDAP_LIBS@
     bld.env.LDAP_LIBS = ''
@@ -177,7 +175,7 @@ def build(bld):
                             dep_vars=['LDB_VERSION'],
                             target='include/ldb_version.h',
                             public_headers='include/ldb_version.h',
-                            public_headers_install=not private_library)
+                            public_headers_install=not bld.env.ldb_is_public_library)
     t.env.LDB_VERSION = LDB_VERSION
 
     bld.SAMBA_MODULE('ldb_asq',

commit ba4a96b2b9658b16434eefc215a674b829175a49

ldb: Fix ldb public library header files being unusable
An accidental negation means that ldb_version.h is not installed when
ldb is built as a public library.

This is a regression introduced by commit
c6763e26cc2c741351a459bf4a83935628c0ff82.

BUG: https://bugzilla.samba.org/show_bug.cgi?id=15690

Signed-off-by: Jo Sutton <josutton@catalyst.net.nz>
Reviewed-by: Douglas Bagnall <douglas.bagnall@catalyst.net.nz>

Autobuild-User(master): Douglas Bagnall <dbagnall@samba.org>
Autobuild-Date(master): Sun Aug  4 01:35:55 UTC 2024 on atb-devel-224

wscript
@@ -174,7 +174,7 @@ def build(bld):
                             dep_vars=['LDB_VERSION'],
                             target='include/ldb_version.h',
                             public_headers='include/ldb_version.h',
-                            public_headers_install=not bld.env.ldb_is_public_library)
+                            public_headers_install=bld.env.ldb_is_public_library)
     t.env.LDB_VERSION = LDB_VERSION
 
     bld.SAMBA_MODULE('ldb_asq',